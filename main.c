d91e868226a8797e26e786f8258ff1ff2b004882
shour <shourjjomajum@outlook.com>
2023-06-18 10:16:43 +0530

    added help

--- main.c
+++ main.c
@@ -6,10 +6,14 @@
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        printf("Usage: %s <filename>\n", argv[0]);
+        printf("Usage: %s <filename.crf>\n", argv[0]);
         return 1;
     }
-  
+   if (strcmp(argv[1], "--help") == 0) {
+   printf("A programming language \n");
+        printf("Usage: %s <filename>\n", argv[0]);
+        return 0;
+    }
 
 
     FILE *file = fopen(argv[1], "r");
