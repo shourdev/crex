78755add937d016004ceae21217ec1b4384beb65
shour <shourjjomajum@outlook.com>
2023-06-23 12:10:38 +0530

    fixed some issues

--- main.c
+++ main.c
@@ -12,9 +12,20 @@ char array[MAX_SIZE][MAX_NAME_LENGTH + MAX_VALUE_LENGTH];
 int currentPosition = 0;
 
 void addNameAndValue(const char* name, const char* value) {
-    // Copy the name and value to the array
+    // Copy the name to the array
     strcpy(array[currentPosition], name);
-    strcpy(array[currentPosition + 1], value);
+
+    // Remove quotes from the value before storing it
+    char cleanValue[MAX_VALUE_LENGTH];
+    strncpy(cleanValue, value + 1, strlen(value) - 2);
+    cleanValue[strlen(value) - 2] = '\0';
+
+    // Remove the last quote if it exists
+    if (cleanValue[strlen(cleanValue) - 1] == '"') {
+        cleanValue[strlen(cleanValue) - 1] = '\0';
+    }
+
+    strcpy(array[currentPosition + 1], cleanValue);
 
     // Move the current position to the next pair
     currentPosition += 2;
@@ -42,6 +53,82 @@ const char* findNameAndGetNextValue(const char* name) {
 
     return NULL;
 }
+
+int hasQuotes(const char* str) {
+    int len = strlen(str);
+
+    // Skip leading whitespace
+    int start = 0;
+    while (start < len && isspace(str[start])) {
+        start++;
+    }
+
+    // Skip trailing whitespace
+    int end = len - 1;
+    while (end >= start && isspace(str[end])) {
+        end--;
+    }
+
+    // Check if the remaining string has at least two characters
+    if (end - start >= 1) {
+        // Check if the first and last characters are double quotation marks
+        if (str[start] == '"' && str[end] == '"') {
+            return 1; // String has quotes
+        }
+    }
+
+    return 0; // String does not have quotes
+}
+void removeQuotes(char* str) {
+    int len = strlen(str);
+
+    // Check if the string is long enough to have quotes
+    if (len >= 2 && str[0] == '"' && str[len - 1] == '"') {
+        // Shift characters to remove the quotes
+        for (int i = 1; i < len - 1; i++) {
+            str[i - 1] = str[i];
+        }
+        str[len - 2] = ' '; // Replace the last quote with a space
+        str[len - 1] = '\0'; // Null-terminate the string
+    }
+}
+
+
+
+int isStringCheck(const char* text) {
+    int i;
+
+    for (i = 0; text[i] != '\0'; i++) {
+        if (!isdigit(text[i])) {
+            return 1;  // Not a string
+        }
+    }
+
+    return 0;  // String
+}
+void findNameAndUpdateValue(const char* name, const char* newValue) {
+    for (int i = 0; i < currentPosition; i += 2) {
+        if (strcmp(array[i], name) == 0) {
+            // Check if the position is even
+            if (i % 2 == 0) {
+                // Remove the value at the position
+                for (int j = i; j < currentPosition - 2; j++) {
+                    strcpy(array[j], array[j + 2]);
+                }
+                currentPosition -= 2;
+
+                // Insert the new value at the position
+                strcpy(array[i + 1], newValue);
+                return;
+            } else {
+                printf("Error variable not found.\n");
+                return;
+            }
+        }
+    }
+
+    printf("Error variable not found.\n");
+}
 void removeWord(char *str, const char *word) {
     char *pos = strstr(str, word); // Find the first occurrence of the word
 
@@ -51,40 +138,27 @@ void removeWord(char *str, const char *word) {
 
         pos = strstr(str, word); // Find the next occurrence of the word
     }
-}
-int hasQuotes(const char* str) {
+
+    // Remove leading whitespace
     int len = strlen(str);
-
-    // Check if the string has at least two characters
-    if (len >= 2) {
-        // Check if the first and last characters are double quotation marks
-        if (str[0] == '"' && str[len - 1] == '"') {
-            return 1; // String has quotes
-        }
+    int start = 0;
+    while (isspace((unsigned char)str[start])) {
+        start++;
     }
 
-    return 0; // String does not have quotes
-}
-
-char removeQuotes(char* str) {
-    if (hasQuotes(str) == 1) {
-        int len = strlen(str);
-        hasqu = 1;
-      
-        // Check if the string is long enough to have quotes
-        if (len >= 2 && str[0] == '"' && str[len - 1] == '"') {
-            // Shift all characters one position to the left to overwrite the quotes
-            for (int i = 0; i < len - 2; i++) {
-                str[i] = str[i + 1];
-            }
-
-            // Null-terminate the string to remove the trailing quote
-            str[len - 2] = '\0';
-        }
+    // Remove trailing whitespace
+    int end = len - 1;
+    while (end >= start && isspace((unsigned char)str[end])) {
+        end--;
     }
+
+    // Shift characters to remove leading and trailing whitespace
+    int dst = 0;
+    for (int src = start; src <= end; src++) {
+        str[dst++] = str[src];
+    }
+    str[dst] = '\0'; // Null-terminate the resulting string
 }
-
-
 int main(int argc, char *argv[]) {
     char line[10000];
     FILE *file = fopen(argv[1], "r");
@@ -95,32 +169,7 @@ int main(int argc, char *argv[]) {
     }
 
     while (fgets(line, sizeof(line), file)) {
-        if (strstr(line, "cout:") != NULL) {
-            removeWord(line, "cout:");
-            stripWhitespace(line);
-            if(hasQuotes==1){
-removeQuotes(line);
-printf("%s",line); 
-            }
-            else{
-                
-                const char* val = findNameAndGetNextValue(line);
-               char newString[strlen(val) + 1];  // Add 1 for the null terminator
-
-    // Copy the data to the new array
-    strcpy(newString, val);
-                removeQuotes(newString);
-                printf("%s \n",newString);
-                
-            }
-            
-       
-           
-        }
-        if (strstr(line, "cin:") != NULL) {
-            // Handle cin statements here
-        }
-        if (strstr(line, "var") != NULL) {
+         if (strstr(line, "var") != NULL) {
             char varName[100];
             char value[100];
 
@@ -134,7 +183,7 @@ printf("%s",line);
                     varName[end - pos] = '\0';
                 }
             }
- char varNameCopy[100]; // Copy of varName
+// Copy of varName
          
             // Extracting value
             pos = strchr(line, '=');
@@ -144,13 +193,54 @@ printf("%s",line);
                     pos++; // Skip spaces after "="
                 }
                 strcpy(value, pos);
-                   strcpy(varNameCopy, varName);
-                   stripWhitespace(varName);
- stripWhitespace(value);
+                   
+
    addNameAndValue(varName,value);
       
             }
         }
+   if (strstr(line, "cout:") != NULL) {
+    removeWord(line, "cout:");
+  //  stripWhitespace(line);
+    if (hasQuotes(line) == 1) {
+  
+        removeQuotes(line);
+     
+        printf("%s\n", line);
+    } else {
+        int result = isStringCheck(line);
+
+if (result){
+  
+   const char* val = findNameAndGetNextValue(line);
+        if (val != NULL) {
+            char newString[strlen(val) + 1];  // Add 1 for the null terminator
+            strcpy(newString, val);
+           removeQuotes(newString);
+            
+            printf("%s\n", newString);
+        } else {
+            printf("Variable %s not found.\n",line);
+         break;
+        }
+}
+else {
+    printf("%s\n",line);
+}
+     
+    }
+}
+        if (strstr(line, "cin:") != NULL) {
+            char opt[10000];
+            scanf("%s",opt);
+            removeWord(line,"cin:");
+;
+            stripWhitespace(line);
+            findNameAndUpdateValue(line,opt);
+            
+        }
+       
+
     }
 
     fclose(file);
