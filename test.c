a49aff04e995358ae124a8f011f356f4b000e24e
shour <shourjjomajum@outlook.com>
2023-07-20 20:55:45 +0530

    completed type conversion

--- test.c
+++ test.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
-#include <ctype.h>
 
 int main() {
-    char input_string[100]; // Assuming the input will not exceed 100 characters
-    scanf("%s", input_string);
+    float floatValue = 42.75;
+    int integerValue;
 
-    int is_numeric = 1;
-    for (char* ptr = input_string; *ptr != '\0'; ptr++) {
-        if (!isdigit(*ptr)) {
-            is_numeric = 0;
-            break;
-        }
-    }
+    // Convert the float to an integer
+    integerValue = (int)floatValue;
 
-    printf(is_numeric ? "bye\n" : "hi\n");
+    printf("Float value: %f\n", floatValue);
+    printf("Integer value: %d\n", integerValue);
+
     return 0;
 }
