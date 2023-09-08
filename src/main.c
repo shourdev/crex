7b09b98d93130e9c0943aece92aa50d354872225
shour <shourjjomajum@outlook.com>
2023-09-08 20:10:47 +0530

    idk

--- src/main.c
+++ src/main.c
@@ -51,59 +51,57 @@ int main()
     FL = fopen("main.crf", "r");
 
     char *file_content = read_file("main.crf");
-Token* tokens;
-int num_tokens = 0;
-lexer(file_content,&tokens,&num_tokens);
-for (int i=0;i<num_tokens;i++){
-    switch (tokens[i].type)
+    Token *tokens;
+    int num_tokens = 0;
+    lexer(file_content, &tokens, &num_tokens);
+    for (int i = 0; i < num_tokens; i++)
     {
-    case INT:
-        printf("Token: INT LITERAL, Value: %s\n",tokens[i].value);
-        
-        break;
-    case STRING:
-        printf("Token: STRING LITERAL, Value: %s\n",tokens[i].value);
-        break;
-    case FLOAT:
-        printf("Token: FLOAT_LITERAL, Value: %s\n",tokens[i].value);
-        break;
-    case PLUS_OP:
-        printf("Token: PLUS_OPERATOR\n");
-        break;
-    case INT_KEY:
-        printf("Token: INT_KEYWORD\n");
-        break;
-    case EQUAL:
-        printf("Token: Equals\n");
-        break;
-    case IDENTFIER:
-        printf("Token: Identifier, Value: %s\n",tokens[i].value);
-        break;
-    case STRING_KEY:
-        printf("Token: STRING_KEYWORD\n");
-        break;
-    case MUL_OP:
-        printf("Token: MULTIPLY_OPERATOR\n");
-        break;
-    case OPEN_PAREN:
-        printf("Token: OPEN_PAREN\n");
-        break;
-    case CLOSE_PAREN:
-        printf("Token: CLOSE_PAREN\n");
-        break;
-    case OVER_KEY:
-        printf("Token: OVER_KEYWORD\n");
-        break;
-    case COMMA:
-        printf("Token: Comma\n");
-        break;
-    case TOKEN_EOF:
-        printf("Token: EOF \n");
-        break;
-    
-    
-   
+        switch (tokens[i].type)
+        {
+        case INT:
+            printf("Token: INT LITERAL, Value: %s\n", tokens[i].value);
+
+            break;
+        case STRING:
+            printf("Token: STRING LITERAL, Value: %s\n", tokens[i].value);
+            break;
+        case FLOAT:
+            printf("Token: FLOAT_LITERAL, Value: %s\n", tokens[i].value);
+            break;
+        case PLUS_OP:
+            printf("Token: PLUS_OPERATOR\n");
+            break;
+        case INT_KEY:
+            printf("Token: INT_KEYWORD\n");
+            break;
+        case EQUAL:
+            printf("Token: Equals\n");
+            break;
+        case IDENTFIER:
+            printf("Token: Identifier, Value: %s\n", tokens[i].value);
+            break;
+        case STRING_KEY:
+            printf("Token: STRING_KEYWORD\n");
+            break;
+        case MUL_OP:
+            printf("Token: MULTIPLY_OPERATOR\n");
+            break;
+        case OPEN_PAREN:
+            printf("Token: OPEN_PAREN\n");
+            break;
+        case CLOSE_PAREN:
+            printf("Token: CLOSE_PAREN\n");
+            break;
+        case OVER_KEY:
+            printf("Token: OVER_KEYWORD\n");
+            break;
+        case COMMA:
+            printf("Token: Comma\n");
+            break;
+        case TOKEN_EOF:
+            printf("Token: EOF \n");
+            break;
+        }
     }
-}
-free(tokens);
+    free(tokens);
 }
