127dc61de132e489b2757c6a0ebb4d418df4acd2
shour <shourjjomajum@outlook.com>
2023-11-11 19:16:25 +0530

    idk

--- src/code.py
+++ src/code.py
@@ -1,17 +1,7 @@
 from stdlib import*
-class intnode:
-    num = None
-class addnode:
-    left = None
-    right = None
-class ast_node:
-    node = None
-intno =intnode()
-intno.num=2
-intno2 =intnode()
-intno2.num=2
-add =addnode()
-add.left=intno
-add.right=intno2
-node =ast_node()
-ast_node.node=add
+def test():
+    cout("Hello outer!")
+    def test2():
+       cout("hello inner!")
+    return test2
+test()()
